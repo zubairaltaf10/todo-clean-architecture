1118e3e5b41ba6e90fb6d1dd84f740e6e937cb9b
Zubair <zubairaltaf10@gmail.com>
2021-10-01 17:27:29 +0500

    release 1.1

--- http/config/index.ts
+++
@@ -1,8 +0,0 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-
-import express from "./express";
-import googleConfig from "./googleConfig";
-import database from './database.js'
-console.log(database)
-export default {express,database,googleConfig}
